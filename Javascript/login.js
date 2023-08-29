9ed8d82efed84fc2d3e68db52526c6f15172a5ff
joninho-jun <141237573+joninho-jun@users.noreply.github.com>
2023-08-30 01:34:31 +0900

    login画面のjavascriptを編集
    暗証番号の登録ロジックを入れた

--- Javascript/login.js
+++ Javascript/login.js
@@ -4,6 +4,7 @@ const password = ["","",""]
 const plus_btn = document.querySelectorAll(".plus_btn");
 const minus_btn = document.querySelectorAll(".minus_btn");
 const reset_btn = document.getElementById("reset");
+const login_btn = document.getElementById("login");
 for(let i =0;i<number.length;i++){
     plus_btn[i].addEventListener("click",()=>{
         if(number_array[i]===9){
@@ -33,3 +34,15 @@ reset_btn.addEventListener("click",()=>{
         number[i].textContent = number_array[i];
     }
 });
+login_btn.addEventListener("click",()=>{
+    auth_word=[
+        "PMM",
+        "PMM",
+        "PMM"
+    ];
+    if(auth_word[0]===password[0] && auth_word[1]===password[1] && auth_word[2]===password[2]){
+        location.href="index.html";
+    }else{
+        alert("暗証番号が違います");
+    }
+}); 
